908bcdd72b8ce0d05c7a08c55a14e81d69b58fcf
Marcos Dominguez <mdominguez2010@yahoo.com>
2021-07-24 11:19:26 -0700

    add multiple stocks to api call

--- data/api.py
+++ data/api.py
@@ -5,39 +5,70 @@ import requests
 import pandas as pd
 import numpy as np
 
-# API call
-api_key = 'VHOI1ERJ34C0FKHZ'
-stock = "IBM"
-url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={stock}&outputsize=full&apikey={api_key}"
-data = requests.get(url).json()
+def get_price_data(api_key, stock):
+    """
+    Makes an Alpha Vantage API call.
+    Returns json with open, high, low, close information of specified stock.
+    """
+    api_key = api_key
+    stock = stock
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={stock}&outputsize=full&apikey={api_key}"
+    data = requests.get(url).json()
 
-# Put data into dataframe
-dataframe = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+    return data
 
-for key in data["Time Series (Daily)"].keys():
-#     print(key, "open:",  data["Time Series (Daily)"][key]["1. open"], "high:", data["Time Series (Daily)"][key]["2. high"], "low:",  data["Time Series (Daily)"][key]["3. low"], "close:",  data["Time Series (Daily)"][key]["4. close"], "adj close:",  data["Time Series (Daily)"][key]["5. adjusted close"], "volume:",  data["Time Series (Daily)"][key]["6. volume"])
-    dataframe = dataframe.append(
-        pd.Series(
-            [key,
-            data["Time Series (Daily)"][key]["1. open"],
-            data["Time Series (Daily)"][key]["2. high"],
-            data["Time Series (Daily)"][key]["3. low"],
-            data["Time Series (Daily)"][key]["4. close"],
-            data["Time Series (Daily)"][key]["5. adjusted close"],
-            data["Time Series (Daily)"][key]["6. volume"]], index=dataframe.columns
-        ), ignore_index=True
-    )
+def create_dataframe(data):
+    """
+    Puts the price data into a dataframe and cleans it
+    """
+    # Put data into dataframe
+    dataframe = pd.DataFrame(columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])       
 
-# Clean up dataframe and save to csv
-dataframe.sort_values("Date", ascending=True, inplace=True, ignore_index=True)
-dataframe.to_csv(f"./data/{stock}.csv", index=False)
+    for key in data["Time Series (Daily)"].keys():
+        dataframe = dataframe.append(
+            pd.Series(
+                [key,
+                data["Time Series (Daily)"][key]["1. open"],
+                data["Time Series (Daily)"][key]["2. high"],
+                data["Time Series (Daily)"][key]["3. low"],
+                data["Time Series (Daily)"][key]["4. close"],
+                data["Time Series (Daily)"][key]["5. adjusted close"],
+                data["Time Series (Daily)"][key]["6. volume"]], index=dataframe.columns
+            ), ignore_index=True
+        )
+
+    # Clean up dataframe
+    dataframe.sort_values("Date", ascending=True, inplace=True, ignore_index=True)
+
+    return dataframe
+
+def save_csv(dataframe, stock):
+    """
+    Saves dataframe as CSV in current directory
+    """
+    dataframe.to_csv(f"./data/{stock}.csv", index=False)
 
 ### Run this on multiple stocks ###
 
-stock_list = [
-    'SPY', 'QQQ', 'RUT', 'DIA', 'AAPL',
-    'BAC', 'DE', 'EWZ', 'FXE', 'IBB',
-    'IWM', 'SLV', 'GLD', 'T', 'TSLA',
-    'WFC', 'FSLR', 'IBM', 'MSFT', 'GOOG']
+if __name__ == '__main__':
 
+    api_key = 'VHOI1ERJ34C0FKHZ'
+    stock_list = stocks = [
+        'SPY', 'QQQ', 'DIA', 'AAPL',
+        'BAC', 'DE', 'EWZ', 'FXE', 'IBB',
+        'IWM', 'SLV', 'GLD', 'T', 'TSLA',
+        'WFC', 'FSLR', 'IBM', 'MSFT', 'V',
+        'NFLX', 'PYPL']
+
+    for stock in stock_list:
+
+        data = get_price_data(api_key, stock)
+        
+        dataframe = create_dataframe(data)
+
+        save_csv(dataframe, stock)
+
+    ### API call on RUT not working ###
+    # data = get_price_data(api_key, 'RUT')
+    # print(data)
 
